5d2e0f954928fbdb22eb85ca812383478068a246
charlieocurtis <xaviersays@protonmail.com>
2025-01-17 14:51:51 +0000

    Web app reads uploaded data and generates plot from it

--- src/dash_main.py
+++ src/dash_main.py
@@ -3,66 +3,70 @@ import plotly.graph_objects as go
 import numpy as np
 import dash_bootstrap_components as dbc
 from dash import Dash, html, dcc, callback, Output, Input
-from dataclasses import dataclass
 
-@dataclass
+
 class PlotConfig:
-    filename: str
-    uploaded_data: np.ndarray
+    def __init__(self):
+        self.filename: str = ""
+        self.uploaded_data: np.ndarray = np.empty((0, 0), dtype=int)
+        self.plot_types: list[str] = ["3D Surface", "2D Contour"]
 
 
+plot_config = PlotConfig()
 
 EXTERNAL_STYLESHEETS = [dbc.themes.SPACELAB]
 app = Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS)
 
-BASE_FILE = "C:\\Users\\Charlie\\PycharmProjects\\bathymetryplot\\Bathymetry-Plot\\data\\gebco_2024_n52.8999_s52.15_w-4.7859_e-4.0361.asc"
+
 ALTERNATE_FILE = "C:\\Users\\Charlie\\Downloads\\GEBCO_14_Jan_2025_6b627da6f9d9\\gebco_2024_n36.1642_s35.7852_w-38.1665_e-37.1393.asc"
-
-with open(BASE_FILE, 'r') as file:
-    original_data = np.loadtxt(file, dtype=int, skiprows=6)
-
 with open(ALTERNATE_FILE, 'r') as file:
     control_data = np.loadtxt(file, dtype=int, skiprows=6)
 
-plot_dict = {"3D Surface":go.Figure(data=go.Surface(z=original_data)),
-             "2D Contour": go.Figure(data=go.Surface(z=control_data))}
-
-uploaded_file: str = ""
-uploaded_data: np.ndarray = np.empty((0, 0), dtype=int)
 
 # App layout
 app.layout = [
     html.H1("Bathymetry-Plot"),
     html.Hr(),
-    html.H2("Upload File:"),
-    dcc.Upload(id="upload_data",children="Drag and drop or click to select a file to upload:"),
-    html.Div(id="uploaded_file"),
+    html.H3("Upload File:"),
+    dcc.Upload(html.Button("Upload File"), id="upload_data_button"),
+    html.Div(id="uploaded_filename"),
     html.Br(),
+    html.H3("Data Snippet:"),
     html.Div(id='recovered_data'),
     html.Br(),
-    dcc.RadioItems(options=list(plot_dict.keys()), value=list(plot_dict.keys())[0], id='controls-and-radio-item'),
+    html.H3("Plot Type: "),
+    dcc.Dropdown(options=plot_config.plot_types, id='dropdown_options'),
     dcc.Graph(figure={}, id='controls-and-graph')
 ]
 
+
 @callback(
     Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
+    Input(component_id='dropdown_options', component_property='value')
 )
 def update_graph(plot_chosen):
-    return plot_dict[plot_chosen]
+    global control_data
+    if plot_chosen == '3D Surface':
+        fig = go.Figure(go.Surface(z=plot_config.uploaded_data))
+    elif plot_chosen == '2D Contour':
+        fig = go.Figure(go.Surface(z=control_data))
+    else:
+        fig = go.Figure()
+    return fig
 
 
 @callback(
-    [Output(component_id='uploaded_file', component_property='children'), Output(component_id='recovered_data', component_property='children')],
-    [Input(component_id='upload_data', component_property='filename'), Input(component_id='upload_data', component_property='contents')]
+    [Output(component_id='uploaded_filename', component_property='children'),
+     Output(component_id='recovered_data', component_property='children')],
+    [Input(component_id='upload_data_button', component_property='filename'),
+     Input(component_id='upload_data_button', component_property='contents')]
 )
 def get_file_location(filename, file_contents):
-    global uploaded_file
-    global uploaded_data
-    uploaded_file = filename
+    plot_config.filename = filename
     decoded = base64.b64decode(str(file_contents)[37:]).decode('utf-8').strip().split('\n')
-    uploaded_data = np.loadtxt(decoded, dtype=int, skiprows=6)
-    return uploaded_file, np.array2string(uploaded_data)
+    plot_config.uploaded_data = np.loadtxt(decoded, dtype=int, skiprows=6)
+    return plot_config.filename, np.array2string(plot_config.uploaded_data)
+
 
 # Run the app
 if __name__ == '__main__':
